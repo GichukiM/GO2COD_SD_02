e1ae6b047b87efb79cef4b16a9bea57eed088a5f
Gichuki Muchiri <119453651+GichukiM@users.noreply.github.com>
2024-11-07 18:36:18 +0300

    Update navbar.jsx

--- frontend-f-calculator/src/components/navbar.jsx
+++ frontend-f-calculator/src/components/navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="../../public/UbuntuCalcHub.webp"
+            src="/UbuntuCalcHub.webp"
             className="w-24 h-24"
             alt="Logo"
           />
